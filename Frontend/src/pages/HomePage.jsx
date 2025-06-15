02b718acde7cff8bd1a53ca787a7ebc1f2fcd547
Felicien407 <felicien407@gmail.com>
2025-06-15 20:00:05 +0200

    Fixed frontend

---
+++ Frontend/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import toast from "react-hot-toast";
+import Navbar from "../components/navbar";
+
+const HomePage = () => {
+  return (
+    <>
+      <div className="min-h-screen">
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
